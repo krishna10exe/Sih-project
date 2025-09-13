11ac9cdf54027becf71ca3ee46a266137a42ca61
ghostfreak10 <codewithghostfreak@gmail.com>
2025-09-13 22:19:51 +0530

    till down Page

--- script.js
+++ script.js
@@ -9,8 +9,41 @@ var tl = gsap.timeline({scrollTrigger:{
 
 tl.to("#solo",{
     top: "120%",
-    right: "100%",
+    right: "108%",
     scale: 1.5,
     rotate: -5,
     ease: "power1.out"
 })
+
+  ScrollReveal().reveal('.d1', {
+    duration: 3000,
+    origin: 'top',
+    distance: '50px',
+    // reset: true
+  });
+
+    ScrollReveal().reveal('.d2', {
+    duration: 3000,
+    origin: 'top',
+    distance: '50px',
+    // reset: true
+  });
+
+    ScrollReveal().reveal('.side', {
+    duration: 3000,
+    origin: 'right',
+    distance: '50px',
+    // reset: true
+  });
+    ScrollReveal().reveal('.about-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '50px',
+    // reset: true
+  });
+    ScrollReveal().reveal('.left', {
+    duration: 2000,
+    origin: 'top',
+    distance: '50px',
+    reset: true
+  });
